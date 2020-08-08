256273fe48341c37e6932bfab218bbceca9d500b
Sean LaFlam <sean.laflam@gmail.com>
2020-08-08 17:19:50 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,34 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+  ]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win|
+    win_index_1 = win[0]
+    win_index_2 = win[1]
+    win_index_3 = win[2]
+    
+    pos1 = board[win_index_1]
+    pos2 = board[win_index_2]
+    pos3 = board[win_index_3]
+    
+    if pos1 == pos2 && pos2 == pos3 
+      return win 
+    else
+      return false
+    end
+  end
+end
+
+    
